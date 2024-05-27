8a566a853e6cc277c6d3a29f5d8d045574f18e45
Gabriela Nunes <152087868+Gabiz3@users.noreply.github.com>
2024-05-27 22:21:01 +0000

    testeg1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-project-rick-and-morty",
+  base: "/Projeto-Rick-And-Morty-React",
   plugins: [react()],
 })
