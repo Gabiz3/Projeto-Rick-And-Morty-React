ecffbee0f4d2458d223f96e0c6be58876e422203
Gabriela Nunes <152087868+Gabiz3@users.noreply.github.com>
2024-05-27 22:17:27 +0000

    teste5

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineCOnfig({
-	base: "/my-project-rick-and-morty",
-	plugins: [react()],
-	})
+export default defineConfig({
+  base: "/my-project-rick-and-morty",
+  plugins: [react()],
+})
